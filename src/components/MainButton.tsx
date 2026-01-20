6b93812bec229a3d4da739111c132e73fbaae4fa
So <nghik720177@gmail.com>
2026-01-20 20:15:24 +0700

    feat/mobile: expert section

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -8,7 +8,7 @@ interface MainButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> 
 }
 
 const MainButton: React.FC<MainButtonProps> = ({ to, className = '', children, title, ...props }) => {
-  const baseClasses = `bg-black text-white font-bold font-nunito py-4 px-4 lg:px-8 rounded-md text-base lg:text-lg hover:bg-gray-800 transition-colors w-full lg:w-auto lg:min-w-[460px] flex justify-center items-center text-center ${className}`;
+  const baseClasses = `bg-black text-white font-bold font-nunito py-4 px-4 md:px-8 rounded-md text-base md:text-lg hover:bg-gray-800 transition-colors w-full md:w-auto md:min-w-[460px] flex justify-center items-center text-center ${className}`;
 
   if (to) {
     return (
--- src/sections/ExpertSection.tsx
+++ src/sections/ExpertSection.tsx
@@ -2,17 +2,17 @@ import { MainButton, MoneyBackGuarantee } from '../components';
 
 const ExpertSection = () => {
   return (
-    <section className='bg-white py-8 lg:py-16'>
+    <section className='bg-white py-8 md:py-16'>
       <div className='page-container'>
         <div className='grid grid-cols-1 md:grid-cols-2 overflow-hidden rounded-lg bg-[#F8F8F8]'>
-          <div className='flex w-full flex-col justify-center p-4  lg:p-8 order-2 md:order-1'>
+          <div className='flex w-full flex-col justify-center p-4  md:p-8 order-2 md:order-1'>
             <span className='mb-4 text-base font-nunito font-medium text-sculptique-muted leading-[1.3]'>July 4th, 2025</span>
 
-            <h2 className='mb-4 font-lora text-3xl font-medium leading-tight text-sculptique-text lg:text-[32px]'>
+            <h2 className='mb-4 font-lora text-2xl font-medium  text-sculptique-text md:text-[32px] leading-[1.3]'>
               Expert Advice from Dr. Emily Chen of a Premier New York Skin Clinic
             </h2>
 
-            <div className='space-y-2 font-nunito text-base leading-[1.3] lg:text-lg'>
+            <div className='space-y-2 font-nunito text-lg leading-[1.3] md:text-lg'>
               <p>
                 “Your nutrition plays a powerful role in your appearance. If your body is missing key vitamins and nutrients, it can’t
                 process fat and upkeep healthy connective tissue, which is why unprocessed fat flows up and starts pushing up against your
@@ -24,7 +24,7 @@ const ExpertSection = () => {
               </p>
             </div>
 
-            <div className='mt-4 flex flex-col items-start gap-4'>
+            <div className='mt-5 flex flex-col items-start gap-4'>
               <MainButton title='Try Lymphatic Drainage Risk-Free' to='https://pay.trysculptique.com/lymphatic/checkout' />
 
               <div className='w-full flex items-center justify-center'>
@@ -33,7 +33,7 @@ const ExpertSection = () => {
             </div>
           </div>
 
-          <div className='relative w-full lg:w-full bg-[#F3EEE0] min-h-[345px] lg:min-h-[400px] h-full order-1 md:order-2 rounded-lg overflow-hidden'>
+          <div className='relative w-full md:w-full  min-h-[283px] md:min-h-[400px] h-full order-1 md:order-2 rounded-lg overflow-hidden'>
             <img
               src='https://trysculptique.com/cdn/shop/files/greendeskt-min.png?v=1758713213'
               alt='Dr. Emily Chen'
