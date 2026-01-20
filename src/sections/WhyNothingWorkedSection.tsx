46b1e8e7018b09e1ea76d9a08cdfb7e03be398c9
So <nghik720177@gmail.com>
2026-01-20 10:55:05 +0700

    feat/mobile: whynothing sections

--- src/sections/WhyNothingWorkedSection.tsx
+++ src/sections/WhyNothingWorkedSection.tsx
@@ -21,25 +21,25 @@ const REASONS = [
 
 const WhyNothingWorkedSection = () => {
   return (
-    <section className='bg-sculptique-yellow py-1 overflow-hidden'>
+    <section className='bg-sculptique-yellow py-0.5 md:py-1 overflow-hidden'>
       <div className='page-container relative'>
-        <h2 className='font-lora text-2xl leading-snug text-sculptique-text lg:text-[32px] lg:leading-[1.3] text-center mb-4 lg:mb-10'>
+        <h2 className='font-lora text-2xl leading-snug text-sculptique-text md:text-[32px] md:leading-[1.3] text-center mb-10'>
           Why Nothing Has <span className='text-sculptique-green'>Worked</span>
         </h2>
 
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-4 relative z-10'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-2 md:gap-4 relative z-10'>
           {REASONS.map((reason, idx) => (
-            <div key={idx} className='flex flex-col h-full bg-white shadow-sm rounded-xl overflow-hidden'>
+            <div key={idx} className='flex flex-col h-full bg-[#f7f7f7] shadow-sm rounded-lg overflow-hidden'>
               <div className='w-full relative'>
                 <img src={reason.image} alt={reason.title} className='w-full h-auto object-cover block' loading='lazy' />
               </div>
 
-              <div className='flex flex-col items-center text-center flex-grow p-6'>
+              <div className='flex flex-col items-center text-center flex-grow p-4 md:p-6'>
                 <h3 className='font-trirong text-xl leading-[1.3] font-medium text-sculptique-text mb-5 px-1 min-h-[3rem] flex items-center justify-center'>
                   {reason.title}
                 </h3>
 
-                <div className='w-full bg-[#ffe3e3] rounded-lg px-2 py-4 flex flex-col items-center flex-grow relative group'>
+                <div className='w-full bg-[#ffe3e3] rounded-lg px-2 py-2 md:py-4 flex flex-col items-center flex-grow relative group'>
                   <div className='flex items-center mb-3'>
                     <span className='font-nunito text-base font-semibold text-sculptique-text'>Why it failed</span>
                     <img
@@ -48,24 +48,29 @@ const WhyNothingWorkedSection = () => {
                       className='w-[24px] h-[25px]'
                     />
                   </div>
-                  <p className='font-nunito text-base leading-relaxed text-sculptique-text font-bold px-2'>{reason.reason}</p>
+                  <p className='font-nunito text-base text-sculptique-text font-bold px-4 mb-2'>{reason.reason}</p>
                 </div>
               </div>
             </div>
           ))}
         </div>
 
-        <div className='relative z-10 hidden lg:flex justify-center'>
+        <div className='relative z-10 py-6 md:py-0 md:flex justify-center'>
           <img
             src='https://trysculptique.com/cdn/shop/files/Group_7207_1.png?v=1760694176'
             alt='The Missing Piece'
-            className='w-full max-w-[920px]'
+            className='hidden md:block w-full max-w-[920px]'
+          />
+          <img
+            src='https://trysculptique.com/cdn/shop/files/Frame_1000003583_1.png?v=1760694176'
+            alt='The Missing Piece Mobile'
+            className='block md:hidden w-full max-w-[920px]'
           />
         </div>
 
-        <div className='flex flex-col items-center mt-4 lg:mt-6 gap-5 mb-16'>
+        <div className='flex flex-col items-center md:mt-6 gap-5 mb-16'>
           <MainButton title='Try Lymphatic Drainage Risk-Free' to='https://pay.trysculptique.com/lymphatic/checkout' />
-          <MoneyBackGuarantee bold />
+          <MoneyBackGuarantee bold size='h-6 w-6' />
         </div>
       </div>
     </section>
